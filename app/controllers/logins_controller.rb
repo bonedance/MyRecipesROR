852a42f31aed1ca4e084a2efdfbfcfca5eb328af
bonedance <bonedance2222@yahoo.com>
2015-08-23 15:55:12 +0000

    Created Chefs resources and added authentication

---
+++ app/controllers/logins_controller.rb
@@ -0,0 +1,34 @@
+class LoginsController < ApplicationController
+  
+  def new
+    
+  end
+  
+  def create
+    
+    chef = Chef.find_by(email: params[:email])
+    
+    if chef && chef.authenticate(params[:password])
+      
+      session[:chef_id] = chef.id
+      flash[:success] = "You're logged in ;)"
+      redirect_to recipes_path
+      
+    else
+      
+      flash.now[:danger] = "Yor email or password does not match"
+      render 'new'
+      
+    end
+    
+  end
+  
+  def destroy
+    
+    session[:chef_id] = nil
+    flash[:success] = "You have logged out"
+    redirect_to root_path
+    
+  end
+  
+end
