da5dce33eceaf09f35b67c641b7d30e53224d974
bonedance <bonedance2222@yahoo.com>
2015-08-27 15:50:29 +0000

    Delete for admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825195503) do
+ActiveRecord::Schema.define(version: 20150827143711) do
 
   create_table "chefs", force: :cascade do |t|
     t.string   "chefname"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150825195503) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   create_table "ingredient_recipes", force: :cascade do |t|
